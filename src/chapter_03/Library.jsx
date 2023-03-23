2b8959f7cde943854632c32a6df4fcd54d09d980
Yang Hyemin <asitwas729@gmail.com>
2023-03-23 17:15:19 +0900

    CHAPTER_03  실습 업로드

--- src/chapter_03/Library.jsx
+++ src/chapter_03/Library.jsx
@@ -1,15 +1,14 @@
-import { func } from "prop-types";
 import React from "react";
 import Book from "./Book";
 
-function Library(props){
-  return(
+function Library(props) {
+  return (
     <div>
-      <Book name = "처음 만난 파이썬" numOfPage = {300}></Book>
-      <Book name = "처음 만난 AWS" numOfPage = {400}></Book>
-      <Book name = "처음 만난 리액트" numOfPage = {500}></Book>
+      <Book name="처음 만난 파이썬" numOfPage={300} />
+      <Book name="처음 만난 AWS" numOfPage={400} />
+      <Book name="처음 만난 리엑트" numOfPage={500} />
     </div>
-  )
+  );
 }
 
 export default Library;
