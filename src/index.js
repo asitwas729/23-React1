389eb42567a7fbb07dab7bbc8f8457271a36ddce
Yang Hyemin <asitwas729@gmail.com>
2023-04-06 15:18:09 +0900

    첫 컴포넌트

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from './chapter_03/Library';
+import Comment from './chapter_05/Comment';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library />
+    <Comment />
   </React.StrictMode>
 );
 
